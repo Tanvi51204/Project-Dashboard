b5f99dd42b5548544d1826e63df34014001a4f3a
Tanvi51204 <tanvibisht2004@gmail.com>
2024-04-01 21:02:14 +0530

    1st april

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const {Pool} = require('pg');
+
 const router = express.Router();
 const app=express();
 const port=3000;
+
+
 
 app.use(bodyParser.json());
 app.use(express.static('views'));
@@ -20,37 +24,6 @@ const pool= new Pool(
     }
 );
 pool.connect();
-
-//not being used 👇🏻
-app.get('/test', async (req, res) => {
-    try {
-        // Execute the test query
-        const query = 'SELECT * FROM public.hospitals LIMIT 5';
-        const result = await pool.query(query);
-
-        // Send the query result as a response
-        res.json(result.rows);
-    } catch (error) {
-        console.error('Error executing test query:', error);
-        res.status(500).send('Internal server error');
-    }
-});
-
-
-app.get('/', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM public.fireincident'); 
-        const locations = result.rows;
-        client.release();
-
-        // Render the map HTML page and pass the location data to it
-        res.render('fireincident', { locations });
-    } catch (error) {
-        console.error('Error fetching data from database:', error);
-        res.status(500).send('Internal server error');
-    }
-});
 
 app.get('/hospitals', async(req,res)=>{
     try{
@@ -92,16 +65,16 @@ app.get('/firestation', async(req,res)=>{
     }
 })
 
-app.get('/map', async (req, res) => {
+app.get('/', async (req, res) => {
     try {
         const client = await pool.connect();
 
-        // Query data from each table
+        
         const hospitalsResult = await client.query('SELECT * FROM public.hospitals');
         const fireStationsResult = await client.query('SELECT * FROM public.firestations');
         const fireIncidentsResult = await client.query('SELECT * FROM public.fireincident');
 
-        // Combine data into a single dataset
+        
         const hospitals = hospitalsResult.rows.map(row => ({
             Name: row.Name,
             HospitalNo : row.HospitalNo,
@@ -125,12 +98,12 @@ app.get('/map', async (req, res) => {
             type: 'fire_incident'
         }));
 
-        // Combine all datasets into a single array
+        
         const combinedData = [...hospitals, ...fireStations, ...fireIncidents];
 
         client.release();
 
-        // Render the map.ejs view and pass the combined data to it
+        
         res.render('map', { locations: combinedData });
         
     } catch (error) {
@@ -142,7 +115,7 @@ app.get('/map', async (req, res) => {
 app.post('/addLoc', (req, res) => {
     const {sql, values} = req.body;
 
-    // Insert data into the database
+    
     pool.query(sql, values, 
                (error, results) => {
         if (error) {
@@ -151,10 +124,12 @@ app.post('/addLoc', (req, res) => {
         } else {
             res.redirect('map.ejs');
             console.log('Location added successfully');
-            //res.status(200).send('Location added successfully');
+            
         }
     });
 });
+
+
 
 
 
